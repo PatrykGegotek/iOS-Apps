71f0454b8d5a5269695bbcf3ab0db9de5c0dd45e
PatrykGegotek <patryk26g@gmail.com>
2023-10-06 10:04:39 +0200

    Initial Commit

---
+++ Lab1/Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1App.swift
+//  Lab1
+//
+//  Created by Guest User on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
